58c34557a35927bf3089e6fffc9f2cb61d741802
cvictor <cvictor@vmware.com>
2021-10-14 14:16:31 -0400

    added reprex

---
+++ src/main/java/com/example/openapigeneratordemo/OpenApiGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapigeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiGeneratorDemoApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(OpenApiGeneratorDemoApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/openapigeneratordemo/OpenApiGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapigeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorDemoApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
